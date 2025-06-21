1fcc8c0ac0811af939ab744d8e027bf4f678435e
Russell <russelldcosta7@gmail.com>
2025-06-21 12:16:36 +0530

    setup fixed

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+engine = create_async_engine(DATABASE_URL, echo=True)   # Create Async Engine
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)   # Session Factory
+Base = declarative_base()   # Base Model
+
+
 
 app = FastAPI()
 
